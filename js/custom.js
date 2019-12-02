a319d6bad2eca70ff8a2924b07d64c31340aa83d
Yurka Litvinov <litvinov.yuriy1996@gmail.com>
2019-12-03 02:05:11 +0400

    Add new pages and refresh old pages

--- js/custom.js
+++ js/custom.js
@@ -3,74 +3,38 @@ $(function() {
     $.datepicker.setDefaults(
         $.extend( $.datepicker.regional[ '' ] )
     );
-    $('#from').daterangepicker({
-        autoUpdateInput: false,
-        locale: {
-            cancelLabel: 'Clear'
-        }
-    });
-    $('#from').on('apply.daterangepicker', function(ev, picker) {
-        $(this).val(picker.startDate.format('MM.DD.YYYY') + ' - ' + picker.endDate.format('MM.DD.YYYY'));
-        $(this).siblings('label').addClass('active');
-    });
-  
-    $('#from').on('cancel.daterangepicker', function(ev, picker) {
-        $(this).val('');
-        $(this).siblings('label').removeClass('active');
-    });
-    $('#birthday').datepicker({dateFormat: "dd.mm.yy"});
+    if ($('#from')) {
+        $('#from').daterangepicker({
+            autoUpdateInput: false,
+            locale: {
+                cancelLabel: 'Clear'
+            }
+        });
+        $('#from').on('apply.daterangepicker', function(ev, picker) {
+            $(this).val(picker.startDate.format('MM.DD.YYYY') + ' - ' + picker.endDate.format('MM.DD.YYYY'));
+            $(this).siblings('label').addClass('active');
+        });
+        $('#from').on('cancel.daterangepicker', function(ev, picker) {
+            $(this).val('');
+            $(this).siblings('label').removeClass('active');
+        });
+    }
+    $('#birthday').datepicker({dateFormat: "dd.mm.yy", autoUpdateInput: false,});
     $('.header__menu img').on('click', function() {
         $('.main__aside').toggleClass('active');
     });
     $("#phone").mask("+7 (999) 999-9999");
 
     // label floating
-    $('.main__content_search .search__row input').on('focus', function() {
+    $('.search__row input').on('focus', function() {
         $(this).siblings('label').addClass('active');
     });
-    $('.main__content_search .search__row input').on('blur', function() {
+    $('.search__row input').on('blur', function() {
         if (!$(this).val()) {
             $(this).siblings('label').removeClass('active');
         }
     });
-    $('.main__content_search .search__row select').on('click', function() {
-        if ($(this).siblings('label').hasClass('active')) {
-            if (!$(this).val()) {
-                $(this).siblings('label').removeClass('active');
-            }
-        } else {
-            $(this).siblings('label').addClass('active');
-        }
-    });
-    $('.popup-partner__body .search__row input').on('focus', function() {
-        $(this).siblings('label').addClass('active');
-    });
-    $('.popup-partner__body .search__row input').on('blur', function() {
-        if (!$(this).val()) {
-            $(this).siblings('label').removeClass('active');
-        }
-    });
-    $('.popup-partner__body .search__row select').on('click', function() {
-        if ($(this).siblings('label').hasClass('active')) {
-            if (!$(this).val()) {
-                $(this).siblings('label').removeClass('active');
-            }
-        } else {
-            $(this).siblings('label').addClass('active');
-        }
-    });
-
-    $('.sign-in__item input').on('focus', function() {
-        console.log('asdasdasd');
-        $(this).siblings('label').addClass('active');
-    });
-    $('.sign-in__item input').on('blur', function() {
-        if (!$(this).val()) {
-            $(this).siblings('label').removeClass('active');
-        }
-    });
-
-    $('.sign-in__item .search__row select').on('click', function() {
+    $('.search__row select').on('click', function() {
         if ($(this).siblings('label').hasClass('active')) {
             if (!$(this).val()) {
                 $(this).siblings('label').removeClass('active');
